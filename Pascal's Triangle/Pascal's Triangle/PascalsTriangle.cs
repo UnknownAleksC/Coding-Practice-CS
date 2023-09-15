79a9ad28daacfab7d2daab7cfce041399a45ca6b
UnknownAleksC <aleksander.carlsen00@gmail.com>
2023-09-15 11:06:38 +0200

    Pascals Triangle
    Code that creates a pyramid depending on the number of rows you submit

---
+++ Pascal's Triangle/Pascal's Triangle/PascalsTriangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Pascal_s_Triangle;
+public static class PascalsTriangle
+{
+    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+    {
+        int[][] pyramid = new int[rows][];
+        for (int i = 0; i < rows; i++)
+        {
+            var previousRow = i == 0 ? new int[1] : pyramid[i - 1];
+            var currentRow = CreateRow(1 + i, previousRow);
+            pyramid[i] = currentRow;
+        }
+        return pyramid;
+    }
+    public static int[] CreateRow(int rowLength, int[] previousRow)
+    {
+        int[] newRow = new int[rowLength];
+        for (int i = 0; i < rowLength; i++)
+        {
+            newRow[i] =
+                i == 0 || i == rowLength - 1 ? 1 : 
+                    previousRow[i] + previousRow[i - 1];
+        }
+        return newRow;
+    }
+}
---
+++ Pascal's Triangle/Pascal's Triangle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pascal_s_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pyramid = PascalsTriangle.Calculate(10);
+        }
+    }
+}
